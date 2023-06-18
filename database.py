5c6576524fffb74dd1ef7103e5c2f8cf9c6da696
hoohep5 <127715439+hoohep5@users.noreply.github.com>
2023-06-18 20:36:06 +1000

    e

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Desk:
 
     def __init__(self, name):
@@ -13,7 +14,6 @@ class Desk:
     def addText(self, name, title, text):
         self.c.execute(f"""INSERT INTO {name} VALUES('{title}', '{text}')""")
         self.db.commit()
-
 
     def returnAbouts(self, name):
         self.c.execute(f"""SELECT about FROM {name}""")
@@ -45,11 +45,12 @@ class Desk:
     def deleteAboutLast(self, name, title, title2):
         self.c.execute(f"""DELETE FROM {name} WHERE {title} = '{title2}'""")
         self.db.commit()
+
     def deleteDesk(self, name):
         self.c.execute(f"""DROP TABLE {name}""")
         self.db.commit()
 
-    def update(self, name, object, object2 ,new):
+    def update(self, name, object, object2, new):
         self.c.execute(f"""UPDATE {name} SET {object} = '{new}' WHERE {object} = '{object2}'""")
         self.db.commit()
 
@@ -57,18 +58,18 @@ class Desk:
         self.c.execute("SELECT name FROM sqlite_master WHERE type='table';")
         return self.c.fetchall()
 
-#DataBace = Desk("desk")
-#DataBace.createNewDesk("Absd")
-#DataBace.addText("Absd", "title1", "text1")
-#DataBace.addText("Absd", "title2", "text2")
-#print(DataBace.returnAll("Absd"))
-#print(DataBace.check("Absd", "title3"))
-#DataBace.deleteAboutLast("Absd", "title", "title1")
-#print(DataBace.returnAll("Absd"))
-#DataBace.addText("Desk1", "Title1", "Text1")
-#Board.addText("Desk1", "Title2", "Text2")
-#Board.deleteAboutDesk("Desk1")
-#Board.addText("Desk1", "Title3", "Text3")
-#print(DataBace.returnAll("New"))
-#print(Board.returnById("Desk1", 2))
-#DataBace.deleteDesk("New")
+# DataBace = Desk("desk")
+# DataBace.createNewDesk("Absd")
+# DataBace.addText("Absd", "title1", "text1")
+# DataBace.addText("Absd", "title2", "text2")
+# print(DataBace.returnAll("Absd"))
+# print(DataBace.check("Absd", "title3"))
+# DataBace.deleteAboutLast("Absd", "title", "title1")
+# print(DataBace.returnAll("Absd"))
+# DataBace.addText("Desk1", "Title1", "Text1")
+# Board.addText("Desk1", "Title2", "Text2")
+# Board.deleteAboutDesk("Desk1")
+# Board.addText("Desk1", "Title3", "Text3")
+# print(DataBace.returnAll("New"))
+# print(Board.returnById("Desk1", 2))
+# DataBace.deleteDesk("New")
